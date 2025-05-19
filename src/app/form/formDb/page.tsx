4235a203f5c95dfed6de95221e586f8f26ca248f
caleb cheruyoit <calebcheruyoit@gmail.com>
2025-05-19 08:35:03 +0300

    everything is okay

--- src/app/form/formDb/page.tsx
+++ src/app/form/formDb/page.tsx
@@ -1,26 +1,9 @@
-import React from 'react'
-import Form from "next/form";
+import React,{} from 'react'
 
 export default function FormDb() {
-
-    const SubmitFunc =() => {
-
-    }
-    return (
-        <div>
-            FormDb
-            <Form {/*action={'/'}*/} onSubmit={SubmitFunc}>
-                <label>
-                    Full name:
-                    <input type={"text"}/>
-                </label>
-                <label>
-                    Age:
-                    <input type={'number'}/>
-                </label>
-                <input type={'submit'}/>
-
-            </Form>
-        </div>
-    )
+  return (
+    <div>FormDb
+        
+    </div>
+  )
 }
